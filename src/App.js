9413f62f23b0171adc6ceddf1c6f40c2b5410bed
Joel Smith <joelwsmith123@gmail.com>
2019-06-29 23:33:18 -0700

    Add basic JSX layout to App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="App">
+  
+        <div className='sidebar'>
+          <nav className='sidebar-nav'>
+  
+          </nav>
+        </div>
+  
+        <div className='notes-container'>
+  
+        </div>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
